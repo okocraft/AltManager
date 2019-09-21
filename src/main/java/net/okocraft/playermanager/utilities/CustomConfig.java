676305c43b602a73b94d25829278ab527ad63951
Siroshun09 <siro.shun8@gmail.com>
2019-09-22 02:24:56 +0900

    [Update] コード整形 (CustomConfig.java)

--- src/main/java/net/okocraft/playermanager/utilities/CustomConfig.java
+++ src/main/java/net/okocraft/playermanager/utilities/CustomConfig.java
@@ -1,15 +1,15 @@
 package net.okocraft.playermanager.utilities;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
-
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.Plugin;
 
 class CustomConfig {
     private FileConfiguration config = null;
